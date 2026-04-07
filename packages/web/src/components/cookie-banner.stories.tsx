5ee243c0909d70edf1c5ab6c469158b1a9698868
Frolda <43851208+frolda@users.noreply.github.com>
2026-04-07 20:47:32 +0800

    storybook fix

--- packages/web/src/components/cookie-banner.stories.tsx
+++ packages/web/src/components/cookie-banner.stories.tsx
@@ -16,7 +16,7 @@ import {
   CookiePreferenceItem,
   CookiePreferencesFooter,
 } from "./cookie-banner"
-import { Button } from "@ragnar/core"
+import { Button } from "@vasf/ragnar-core"
 
 const meta: Meta<typeof CookieBanner> = {
   title: "Web/CookieBanner",
